d6ea429fa155654f962f3cf3fefe32b198797217
Yura Ilyaev <yura.ilyaev@revjet.com>
2018-03-24 11:34:49 -0700

    chore: deploy to heroku

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ const resolvers = {
         godotSnakeServerHost: (parent, args) => {
             return {
                 // host: '192.168.1.11',
-                // host: '165.227.0.101',
-                host: '127.0.0.1',
+                host: '165.227.0.101',
+                // host: '127.0.0.1',
                 port: 4000
             }
         }
